0a89e78eefa4f1628a099aee947f9478d20ac108
Shihab Ahmed <shehabuddin380@gmail.com>
2026-06-22 03:58:13 +0600

    fix api url 2

--- src/services/auth-api-client.js
+++ src/services/auth-api-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApiClient = axios.create({
-  baseURL: "https://phimart.vercel.app/api/v1/",
+  baseURL: "https://phi-mart-pied.vercel.app/api/v1/auth/",
 });
 
 export default authApiClient;
