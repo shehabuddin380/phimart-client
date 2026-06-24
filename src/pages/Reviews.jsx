c1b9466ab278718067cc4a746c9a4b953e8bdb35
Shihab Ahmed <shehabuddin380@gmail.com>
2026-06-25 02:39:22 +0600

    fix reviews

---
+++ src/pages/Reviews.jsx
@@ -0,0 +1,10 @@
+const Reviews = () => {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <h1 className="text-2xl font-bold mb-6">My Reviews</h1>
+      {/* reviews content */}
+    </div>
+  );
+};
+
+export default Reviews;
