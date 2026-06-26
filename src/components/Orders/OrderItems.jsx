fd3e1fc34ed7c22f1604c0dd4573a42e95ea01c6
Shihab Ahmed <shehabuddin380@gmail.com>
2026-06-27 02:57:59 +0600

    fix orderitems

--- src/components/Orders/OrderItems.jsx
+++ src/components/Orders/OrderItems.jsx
@@ -1,6 +1,6 @@
 const OrderItems = ({ item }) => {
   return (
-    <tr className="border-b hover:bg-gray-50">
+    <tr className="border-b border-base-300 hover:bg-base-200">
       <td className="px-4 py-3 font-medium">{item.product.name}</td>
       <td className="px-4 py-3 text-right">${item.price.toFixed(2)}</td>
       <td className="px-4 py-3 text-right">{item.quantity}</td>
